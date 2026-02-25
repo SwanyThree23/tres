78a759a19d9240e7dc4836e7d891ff5e9105c3e2
SwanyThree23 <ljswanson72@gmail.com>
2026-02-24 18:13:29 -0800

    approve

--- cylive/src/app/api/payments/subscribe/route.ts
+++ cylive/src/app/api/payments/subscribe/route.ts
@@ -9,7 +9,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import { subscribeSchema } from "@/schemas";
-import { stripe, VIEWER_SUB_PRICES } from "@/lib/stripe";
+import { VIEWER_SUB_PRICES } from "@/lib/stripe";
 
 export async function POST(req: NextRequest) {
   try {
