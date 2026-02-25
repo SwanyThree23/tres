d6e18333c01135e872a22062588af9d59971946b
SwanyThree23 <ljswanson72@gmail.com>
2026-02-24 18:38:14 -0800

    accept

--- cylive/src/lib/stripe.ts
+++ cylive/src/lib/stripe.ts
@@ -5,8 +5,8 @@
 
 import Stripe from "stripe";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   apiVersion: "2024-04-10" as any,
   typescript: true,
 });
