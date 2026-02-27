d0cd327577b358d8465492288e9260b8e7cf4384
SwanyThree23 <ljswanson72@gmail.com>
2026-02-27 07:17:42 -0800

    approve

--- cylive/next.config.mjs
+++ cylive/next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -28,4 +30,11 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  silent: true,
+  org: "swanythree",
+  project: "cylive",
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+});
